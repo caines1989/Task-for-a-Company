3fd804900077f5968f09b78d9d7c0d16fd7b78fd
caines1989 <6423872+caines1989@users.noreply.github.com>
2022-09-01 23:11:10 +0100

    adding jest tests library and a test

---
+++ backend/controllers/statisticsController.test.js
@@ -0,0 +1,52 @@
+const Statistics = require('../models/statisticsModel')
+const request = require('supertest')
+const mongoose = require('mongoose')
+// const getAllStatistics = async (req, res) => {
+//     const statistics = await Statistics.find({}).sort({createdAt: -1})
+
+//     res.status(200).json(statistics)
+// }
+
+describe('getAllStatistics' , () => {
+    it('returns status 200 when all statistics are available in json', async () => {
+        request(Statistics)
+        .get('/statistics')
+        .expect('Content-Type', /json/)
+        .expect(200)
+    })
+})
+
+describe('getSingleStatistic' , () => {
+    it('returns no such statistic', async () => {
+
+    })
+    it('returns status 200 when single statistics is available in json', async () => {
+        request(Statistics)
+        .get('/statistics')
+        .expect(200)
+    })
+})
+
+describe('createStatistic' , () => {
+    it('returns status 200 when create a statistic in json', async () => {
+        request(Statistics)
+        .get('/statistics')
+        .expect(200)
+    })
+})
+
+describe('deleteStatistic' , () => {
+    it('returns status 200 when deleting a statistic', async () => {
+        request(Statistics)
+        .get('/statistics')
+        .expect(200)
+    })
+})
+
+describe('updateStatistic' , () => {
+    it('returns status 200 when updating statistic', async () => {
+        request(Statistics)
+        .get('/statistics')
+        .expect(200)
+    })
+})
